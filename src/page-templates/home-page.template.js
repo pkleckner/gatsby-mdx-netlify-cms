14b0e7f7fef1cddb8e37c100c6801070398b0a94
Patrick Kleckner <pkleckner@killerpom.com>
2020-05-20 20:47:57 -0400

    Adjusting Home Template Structure to Display MDX Content

--- src/page-templates/home-page.template.js
+++ src/page-templates/home-page.template.js
@@ -1,20 +1,23 @@
 import React from "react"
+import { MDXProvider } from "@mdx-js/react"
 
-import { Heading, SEO } from "../components"
-import { RenderMarkdown } from "../core"
 import {
   safelyGetFrontMatter,
   withFallback,
+  CMS_COMPONENTS,
+  CMS_SHORTCODES,
 } from "../cms"
+import { Heading, SEO } from "../components"
+import { RenderMarkdown } from "../core"
 
-export const HomePageTemplate = ({ title, sections }) => (
+export const HomePageTemplate = ({ title, sections, children }) => (
   <article>
     <SEO title={title} />
     <Heading tag={1}>{title}</Heading>
+    <p>Home Page Template</p>
     {withFallback(sections, []).map((section, i) => {
       return (
         <section key={i}>
-          <p>Home Page Template</p>
           <h2>{section.title}</h2>
           <RenderMarkdown
             md={section.body}
@@ -23,13 +26,17 @@ export const HomePageTemplate = ({ title, sections }) => (
         </section>
       )
     })}
+    <MDXProvider components={{ ...CMS_COMPONENTS, ...CMS_SHORTCODES }}>
+      {children}
+    </MDXProvider>
   </article>
 )
 
-const HomePage = ({ pageContext }) => {
+const HomePage = ({ pageContext, ...props }) => {
   return (
     <HomePageTemplate
       {...safelyGetFrontMatter(pageContext)}
+      {...props}
       isPreview={false}
     />
   )
